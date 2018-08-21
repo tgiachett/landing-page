1c37c90ac29056f4cf1adbb830e139dfc985c06e
tgiachett <31292513+tgiachett@users.noreply.github.com>
2018-08-21 16:13:55 -0700

    Added color transitions and removed orbit, abandoned zoom for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Sun from './components/Orbit/Sun'
+import Profile from './components/Profile/Profile'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Sun />
+        <Profile />
       </div>
     );
   }
