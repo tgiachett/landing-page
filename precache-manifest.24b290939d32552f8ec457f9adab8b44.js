7f6ee1b0539813b093cfb8f72536fb031255a4cf
Tobia Giachetti <tgiachett@gmail.com>
2018-11-08 13:49:03 -0800

    Updates

---
+++ precache-manifest.24b290939d32552f8ec457f9adab8b44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e502b1b7d9d8dee4025",
+    "url": "/landing-page/static/js/runtime~main.5e502b1b.js"
+  },
+  {
+    "revision": "fd3573e726f4a9613039",
+    "url": "/landing-page/static/js/main.fd3573e7.chunk.js"
+  },
+  {
+    "revision": "ae14d192c23deb4dfc8a",
+    "url": "/landing-page/static/js/1.ae14d192.chunk.js"
+  },
+  {
+    "revision": "fd3573e726f4a9613039",
+    "url": "/landing-page/static/css/main.702e0dd1.chunk.css"
+  },
+  {
+    "revision": "95a697929036546795b81ad96f883570",
+    "url": "/landing-page/index.html"
+  }
+];
